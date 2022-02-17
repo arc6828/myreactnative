fc8f42c2c589310c44f8b14210944c13bfabceb8
Chavalit Koweerawong <chavalit.kow@gmail.com>
2022-02-17 21:49:43 +0700

    Week13

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { AuthContextProvider } from "./hooks/AuthContextProvider";
 import BottomTab from "./navigations/BottomTab";
 import HomeStack from './navigations/HomeStack';
 import RootStack from "./navigations/RootStack";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <HomeStack /> */}
-      {/* <BottomTab /> */}
-      <RootStack />
-    </NavigationContainer>
+    <AuthContextProvider>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </AuthContextProvider>
   );
 }
