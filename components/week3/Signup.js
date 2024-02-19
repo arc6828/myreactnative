ddf416d168134a9c22a4e2f7d4db9dfbc55b513b
Chavalit Koweerawong <chavalit.kow@gmail.com>
2024-02-19 14:06:53 +0700

    Update Signup.js

--- components/week3/Signup.js
+++ components/week3/Signup.js
@@ -1,27 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-} from "react-native";
+import React from 'react';
+import { Button, TextInput, View } from 'react-native';
+
 
 export default function Signup() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "stretch",
-        padding: 10,
-      }}
-    >
-      <TextInput style={{ fontSize : 20 , marginTop : 10 }} placeholder="Input ID" />
-      <TextInput style={{ fontSize : 20 , marginTop : 10 }}  placeholder="Input Email" />
-      <TextInput style={{ fontSize : 20 , marginTop : 10 , marginBottom : 10 }}  placeholder="Input Address" />
-      <Button title="Sign UP" color="red" />
-    </View>
-  );
+    return (
+        <View style={{ padding : 20  ,flex: 1, alignItems: "stretch"  }}>
+            <TextInput style={{ fontSize : 20 , marginTop : 10 }} placeholder="Input ID" />
+            <TextInput style={{ fontSize : 20 , marginTop : 10 }}  placeholder="Input Email" />
+            <TextInput style={{ fontSize : 20 , marginTop : 10 , marginBottom : 10 }}  placeholder="Input Address" />
+            <Button title="Sign UP" color="red" />
+        </View>    
+    );
 }
